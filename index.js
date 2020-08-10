649de4521fa5b1c8c97a4cd8be93877d8da01306
Mr Cea <ogbonna.basil3@gmail.com@users.noreply.github.com>
2020-08-10 16:49:01 +0100

    add port

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ dotenv.config()
 
 const server = express();
 server.use(express.urlencoded({extended:true}))
-const port = process.env.port || 3000
+const port = process.env.PORT || 3000
 
 server.listen(port, () => {
   console.log("server running on port 3000");

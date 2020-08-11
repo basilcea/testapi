29b3f1bc011d744c436dd739d8588cb9950eb303
Mr Cea <ogbonna.basil3@gmail.com@users.noreply.github.com>
2020-08-11 09:04:32 +0100

    fixed format

--- index.js
+++ index.js
@@ -16,22 +16,12 @@ server.get('/', (req,res)=> {
 server.post("/validate", async (req, res) => {
   try {
     const {
-      api_key,
-      api_signature,
-      api_type,
-      transaction_reference,
-      transaction_status,
-      transaction_status_code,
-      transaction_status_description,
-      transaction_account,
-      transaction_mobile,
-      trasaction_status_action,
-      transaction_country,
-      transaction_amount,
-      transaction_method,
-      transaction_paybill,
-      transaction_code,
-      transaction_date,
+            remote_transaction_id,
+            phonenumber,
+            reference,
+            amount,
+            currency_code
+    
     } = req.body;
     if (req.body) {
       console.log(req.body);

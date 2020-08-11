fc3130a33016d61c350179413a72ed5abd688146
Mr Cea <ogbonna.basil3@gmail.com@users.noreply.github.com>
2020-08-11 09:08:31 +0100

    fixed format

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ dotenv.config()
 
 const server = express();
 server.use(express.urlencoded({extended:true}))
+server.use(express.json())
 const port = process.env.PORT || 3000
 
 server.listen(port, () => {
